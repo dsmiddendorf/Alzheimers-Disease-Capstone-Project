c69adf85ccb9b778d27e9ee94a50f2b4de3e0c57
Daniel Middendorf <danmid98@gmail.com>
2020-11-20 10:55:16 +0100

    Successfully downloaded the Data.

---
+++ alzheimer.R
@@ -0,0 +1,15 @@
+##########################################################
+# Create alzheimer set, validation set (final hold-out test set)
+##########################################################
+
+# Loading libraries used in later analysis
+if(!require(tidyverse)) install.packages("tidyverse")
+if(!require(caret)) install.packages("caret")
+if(!require(readr)) install.packages("readr")
+
+library(tidyverse)
+library(caret)
+library(readr)
+
+data <- read_csv("https://raw.githubusercontent.com/dsmiddendorf/Alzheimers-Disease-Capstone-Project/main/oasis_cross-sectional.csv")
+
